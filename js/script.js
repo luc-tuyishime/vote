466f3151708607914108822c6a15a2dc4a8b92e8
luc-tuyishime <luctunechi45@gmail.com>
2021-10-27 22:10:56 +0200

    set interval to 1 second

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
-  var number = parseInt($("#test").text(), 10) || 0; // Get the number from paragraph
+$(document).ready(() => {
+  let number = parseInt($("#test").text(), 10) || 0; // Get the number from paragraph
 
   // Called the function in each second
-  var interval = setInterval(function () {
+  let interval = setInterval(() => {
     $("#test").text(number++); // Update the value in paragraph
 
-    if (number > 1000) {
-      clearInterval(interval); // If exceeded 1000, clear interval
-    }
+    return number > 1000 && clearInterval(interval); // If exceeded 1000 clear interval
   }, 1000); // Run for each second
 });
